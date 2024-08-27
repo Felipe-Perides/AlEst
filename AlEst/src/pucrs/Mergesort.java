97992d9b11e4c4b860ad64f8b755cd8164cb4201
Felipe-Perides <f.nicolau@edu.pucrs.br>
2024-08-27 11:04:42 -0300

    Terceiro Commit

--- AlEst/src/pucrs/Mergesort.java
+++ AlEst/src/pucrs/Mergesort.java
@@ -6,7 +6,10 @@ public class Mergesort {
     private int[] data;
     private int[] arr;
     private int number;
-
+    private int contOp;
+    public void zeraOp() {contOp=0;}
+	public int getOp() {return contOp;}
+    
     public void sort(int[] values) {
         this.data = values;
         number = values.length;
@@ -32,6 +35,7 @@ public class Mergesort {
         int p = start, q = mid+1;
         int k=0;
         for(int i=start; i<= end; i++) {
+        	contOp++;
             if(p > mid)
                 arr[k++] = data[q++];
             else if(q > end)
@@ -45,6 +49,45 @@ public class Mergesort {
             data[start++] = arr[p];
     }
 
+    public void sort2(int[] values) {
+        this.data = values;
+        number = values.length;
+        this.arr = new int[number];
+        mergesort2(0, number - 1,"");
+    }
+
+    private void mergesort2(int start, int end, String spaces) {
+        // Verifica se start é menor que end, caso contrário terminou
+        if (start < end) {
+            // Obtém o índice o elemento no meio
+            int mid = (start + end) / 2;
+            // Ordena o lado esquerdo
+            System.out.printf("%sEsq: %d, %d\n",spaces,start,mid);
+            mergesort2(start, mid,spaces+" --");
+            // Ordena o lado direito
+            System.out.printf("%sDir: %d, %d\n",spaces,start,mid);
+            mergesort2(mid + 1, end,spaces+" --");
+            // Combina ambos
+            merge2(start, mid, end);
+        }
+    }
+
+    private void merge2(int start, int mid, int end) {
+        int p = start, q = mid+1;
+        int k=0;
+        for(int i=start; i<= end; i++) {
+            if(p > mid)
+                arr[k++] = data[q++];
+            else if(q > end)
+                arr[k++] = data[p++];
+            else if(data[p] < data[q])
+                arr[k++] = data[p++];
+            else
+                arr[k++] = data[q++];
+        }
+        for(p=0; p<k; p++)
+            data[start++] = arr[p];
+    }
     public static void main(String args[]) {
         Random r = new Random();
         for(int MAX = 10000;MAX<20000;MAX+=1000) {
@@ -54,7 +97,8 @@ public class Mergesort {
 	
 	        long start = System.nanoTime();
 	        Mergesort ms = new Mergesort();
-	        ms.sort(data);
+	        //ms.sort(data);
+	        ms.sort2(data);
 	        long end = System.nanoTime();
 	        System.out.printf("Tempo para ordenar %d elementos: %d ns\n",MAX,end-start);
         }
