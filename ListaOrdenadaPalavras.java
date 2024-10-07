21ddc701f7887fcc992fde5506a74be2531c5369
Felipe-Perides <167124395+Felipe-Perides@users.noreply.github.com>
2024-10-07 12:27:51 -0300

    Commit 07/10/24
    adição do Trabalho 2

---
+++ ListaOrdenadaPalavras.java
@@ -0,0 +1,22 @@
+package trabalho2;
+
+public class ListaOrdenadaPalavras {
+	// Classe interna 
+    private class Palavra {
+        public String s;
+        public ListaOcorrencias listaOcorrencias;
+        public Palavra next;    
+        public Palavra(String str) {
+            s = str;
+            next = null;
+            listaOcorrencias = new ListaOcorrencias();
+        }
+        
+        // Metodos
+
+    }
+    
+    // Atributos
+    
+    // Metodos
+}
